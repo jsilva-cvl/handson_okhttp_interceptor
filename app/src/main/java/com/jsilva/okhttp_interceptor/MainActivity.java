e417f421652792cd6cdb5067c7f40a5a92a58401
Jose Silva <jsilva@codavel.com>
2019-07-18 14:14:37 +0100

    Project skeleton

---
+++ app/src/main/java/com/jsilva/okhttp_interceptor/MainActivity.java
@@ -0,0 +1,13 @@
+package com.jsilva.okhttp_interceptor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
